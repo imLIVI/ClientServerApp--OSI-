bdc127a6e24553fe40a1bd97fee40bdcf652d748
imLIVI <lesly120898@gmail.com>
2022-11-01 21:46:41 +0300

    Remove unused string

--- src/ClientThread.java
+++ src/ClientThread.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class ClientThread extends Thread {
     private final int port = 8080;
